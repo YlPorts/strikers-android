203a0f359c3393f44994b9987eb82a93ce565c2a
Jude <133681884+YlPorts@users.noreply.github.com>
2026-09-14 11:59:36 -0500

    android: trace SDL activity lifecycle

--- android/app/src/main/java/com/ylports/strikers/StrikersActivity.java
+++ android/app/src/main/java/com/ylports/strikers/StrikersActivity.java
@@ -1,6 +1,7 @@
 package com.ylports.strikers;
 
 import android.content.Context;
+import android.os.Bundle;
 
 import dev.encounter.aurora.AuroraSurface;
 import org.libsdl.app.SDLActivity;
@@ -9,7 +10,51 @@ import org.libsdl.app.SDLSurface;
 /** Runs the native Strikers port through SDL3 and Aurora. */
 public final class StrikersActivity extends SDLActivity {
     @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        RunLog.installJavaCrashHandler(this);
+        RunLog.append(this, "SDL activity: onCreate BEFORE SDLActivity.onCreate");
+        super.onCreate(savedInstanceState);
+        RunLog.append(this, "SDL activity: onCreate AFTER SDLActivity.onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        RunLog.append(this, "SDL activity: onStart BEFORE super");
+        super.onStart();
+        RunLog.append(this, "SDL activity: onStart AFTER super");
+    }
+
+    @Override
+    protected void onResume() {
+        RunLog.append(this, "SDL activity: onResume BEFORE super");
+        super.onResume();
+        RunLog.append(this, "SDL activity: onResume AFTER super");
+    }
+
+    @Override
+    protected void onPause() {
+        RunLog.append(this, "SDL activity: onPause BEFORE super");
+        super.onPause();
+        RunLog.append(this, "SDL activity: onPause AFTER super");
+    }
+
+    @Override
+    protected void onStop() {
+        RunLog.append(this, "SDL activity: onStop BEFORE super");
+        super.onStop();
+        RunLog.append(this, "SDL activity: onStop AFTER super");
+    }
+
+    @Override
+    protected void onDestroy() {
+        RunLog.append(this, "SDL activity: onDestroy BEFORE super");
+        super.onDestroy();
+        RunLog.append(this, "SDL activity: onDestroy AFTER super");
+    }
+
+    @Override
     protected String[] getLibraries() {
+        RunLog.append(this, "SDL activity: getLibraries -> strikers");
         // SDL3 is linked statically into libstrikers.so by Aurora, so loading the
         // game library also runs SDL's JNI_OnLoad and registers SDLActivity natives.
         return new String[] { "strikers" };
@@ -17,6 +62,7 @@ public final class StrikersActivity extends SDLActivity {
 
     @Override
     protected SDLSurface createSDLSurface(Context context) {
+        RunLog.append(context, "SDL activity: createSDLSurface -> AuroraSurface");
         return new AuroraSurface(context);
     }
 }
